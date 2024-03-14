a77cadf35d03924a0416df9b9ccbc7ab152c5c2b
Zaim211 <yousseftitazaim@gmail.com>
2024-03-14 17:44:37 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,14 +11,11 @@ def canUnlockAll(boxes):
     if not boxes or type(boxes) is not list:
         return False
 
-    opened = [False] * len(boxes)
-    opened[0] = True
-
-    keys = [0]
-    while keys:
-        box = keys.pop()
-        for key in boxes[box]:
-            if not opened[key] and len(boxes) > 1:
-                opened[key] = True
-                keys.append(key)
-    return all(opened)
+    opened = [0]
+    for n in opened:
+        for key in boxes[n]:
+            if key not in opened and key < len(boxes):
+                opened.append(key)
+    if len(opened) == len(boxes):
+        return True
+    return False
