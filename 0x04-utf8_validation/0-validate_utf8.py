661f6542b51d066fc637e2d658267b8cedb5597a
Zaim211 <yousseftitazaim@gmail.com>
2024-04-05 00:02:02 +0000

    utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+
+def validUTF8(data):
+    """  method that determines if a given data set
+    represents a valid UTF-8 encoding
+    """
+    byte = 0
+
+    for x in data:
+        if byte == 0:
+            if x >> 5 == 0b110 or x >> 5 == 0b1110:
+                byte = 1
+            if x >> 4 == 0b1110:
+                byte = 2
+            if x >> 3 == 0b11110:
+                byte = 3
+            if x >> 7 == 0b1:
+                return False
+        else:
+            if x >> 6 == 0b10:
+                return False
+            byte -= 1
+    return byte == 0
