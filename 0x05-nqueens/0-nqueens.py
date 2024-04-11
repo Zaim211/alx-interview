55e9ff902ef6b96d397ea02b910f983f69cf3672
Zaim211 <yousseftitazaim@gmail.com>
2024-04-11 15:57:51 +0000

    update

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+""" `The N queens puzzle`:
+     the challenge of placing N non-attacking queens
+     on an N×N chessboard.
+     Write a program that solves the N queens problem.
+"""
+import sys
+
+
+def n_queens(N, rows, cols, board):
+    """ Checking the rowon the left side """
+    for x in range(cols):
+        if board[rows][x]:
+            return False
+    """ checking the diagonal on the lef side """
+    for x, y in zip(range(rows, -1, -1), (cols, -1, -1)):
+        if board[x][y]:
+            return False
+    """ checking the lower diagonal on the left side """
+    return all(not board[x][y] for x, y in
+        zip(range(rows, N), range(colss, -1, -1)))
+
+def n_queens_placed(N, board, cols, results):
+    """ checking if are queens are placed and return True """
+    if cols == N:
+        result = []
+        for x in range(N):
+            for y in range(N):
+                if board[x][y]:
+                    result.append([x, y])
+        results.append(result)
+        return True
+
+    solu = False
+    for x in range(N):
+        if n_queens(board, x, cols, N):
+            board[x][cols] = 1
+            solu = n_queens_placed(N, board, cols + 1, results) or solu
+            board[x][cols] = 0
+    return solu
+
+
+def solve_n_queens(N):
+    board = [[0] * N for _ in range(N)]
+    results = []
+
+    if not n_queens_placed(N, board, 0, results):
+        return []
+
+    return results
+
+def main():
+    #If the user called the program with the wrong number of arguments
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    N = sys.argv[1]
+
+    # Check If N is not an integer
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    N = int(N)
+
+    # Check If N is smaller than 4
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    results = solve_n_queens(N)
+
+    # Print the results
+    for result in results:
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
