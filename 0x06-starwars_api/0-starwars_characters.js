f8f0f4130272cf051220fe33aca3144e0490ca48
Zaim211 <yousseftitazaim@gmail.com>
2024-04-17 16:39:51 +0000

    Star Wars Characters Challenge

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-/* script that prints all characters of a Star Wars movie:
-
-The first positional argument passed is
-the Movie ID - example: 3 = “Return of the Jedi”
-Display one character name per line in the same order
-as the “characters” list in the /films/ endpoint
-You must use the Star wars API
-You must use the request module */
 
 const request = require('request');
+
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 let people = [];
@@ -18,11 +11,11 @@ const FilmsEndpoint = async () => {
   await new Promise((resolve) =>
     request(url, (err, res, body) => {
       if (err || res.statusCode != 200) {
-         console.log('Error: ', err, 'StatusCode: ', res.statusCode);
+        console.log('Error: ', err, 'StatusCode: ', res.statusCode);
       } else {
-         const jsonResponse = JSON.parse(body);
-         people = jsonResponse.characters;
-         resolve();
+        const jsonResponse = JSON.parse(body);
+        people = jsonResponse.characters;
+        resolve();
       }
     }));
 };
