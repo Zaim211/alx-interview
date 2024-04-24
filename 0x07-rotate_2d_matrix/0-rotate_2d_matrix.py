b06fd88cdeaf1a2da23d1c02ab2ca7e387ed6720
Zaim211 <yousseftitazaim@gmail.com>
2024-04-24 21:29:27 +0000

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,8 +7,8 @@ def rotate_2d_matrix(matrix):
     degrees clockwise. """
     for row in range(len(matrix)):
         for col in range(row + 1, len(matrix)):
-            matrix[row][col] =  matrix[col][row]
-            matrix[col][row] = matrix[row][col]
+            matrix[row][col], matrix[col][row] = \
+                    matrix[col][row], matrix[row][col]
 
     for rows in matrix:
         rows.reverse()
