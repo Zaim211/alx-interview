7176dc12c5a969981a054c3526de3fa3a8e5bb16
Zaim211 <yousseftitazaim@gmail.com>
2024-05-15 12:40:30 +0000

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,17 +2,21 @@
 """ Prime Game """
 
 
-def Prime_numbers(n):
-    """ Function that check the numbers is Prime
-    with given n.
-    """
-    if n == 1 or n == 0 or (n % 2 == 0 and n > 2):
-        return False
+def isWinner(x, nums):
+    """ Function that find the winner of game """
+    Count_winner = {'Maria': 0, 'Ben': 0}
+
+    for i in range(x):
+        roundWinner = RoundWinner_game(nums[i], x)
+        if roundWinner is not None:
+            Count_winner[roundWinner] += 1
+
+    if Count_winner['Maria'] > Count_winner['Ben']:
+        return 'Maria'
+    elif Count_winner['Ben'] > Count_winner['Maria']:
+        return 'Ben'
     else:
-        for nums in range(3, int(n**(1/2)) + 1, 2):
-            if n % nums == 0:
-                return False
-        return True
+        return None
 
 
 def RoundWinner_game(n, x):
@@ -43,21 +47,17 @@ def RoundWinner_game(n, x):
     return None
 
 
-def isWinner(x, nums):
-    """ Function that find the winner of game """
-    Count_winner = {'Maria': 0, 'Ben': 0}
-
-    for i in range(x):
-        roundWinner = RoundWinner_game(nums[i], x)
-        if roundWinner is not None:
-            Count_winner[roundWinner] += 1
-
-    if Count_winner['Maria'] > Count_winner['Ben']:
-        return 'Maria'
-    elif Count_winner['Ben'] > Count_winner['Maria']:
-        return 'Ben'
+def Prime_numbers(n):
+    """ Function that check the numbers is Prime
+    with given n.
+    """
+    if n == 1 or n == 0 or (n % 2 == 0 and n > 2):
+        return False
     else:
-        return None
+        for nums in range(3, int(n**(1/2)) + 1, 2):
+            if n % nums == 0:
+                return False
+        return True
 
 
 if __name__ == '__main__':
